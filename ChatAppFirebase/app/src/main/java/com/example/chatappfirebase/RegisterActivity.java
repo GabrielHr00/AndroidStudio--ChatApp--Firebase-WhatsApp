b77a8cfe6bd9e2759ee04c37dc95e26416985f8c
gabrielh00 <a11914741@univie.ac.at>
2021-09-04 17:12:39 +0200

    RegisterActivity layout

--- ChatAppFirebase/app/src/main/java/com/example/chatappfirebase/RegisterActivity.java
+++ ChatAppFirebase/app/src/main/java/com/example/chatappfirebase/RegisterActivity.java
@@ -3,12 +3,30 @@ package com.example.chatappfirebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toolbar;
+
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class RegisterActivity extends AppCompatActivity {
+    MaterialEditText et_username, et_password, et_email;
+    Button register;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        toolbar = findViewById(R.id.toolbarreg);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setTitle("Register");
+//        setSupportActionBar(toolbar);
+
+        et_username = findViewById(R.id.reg_username);
+        et_email = findViewById(R.id.reg_email);
+        et_password = findViewById(R.id.reg_password);
+        register = findViewById(R.id.register);
+
     }
 }
