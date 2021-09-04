03c5ecaf307c25b3cca8a8b9b094d98be858f7a5
gabrielh00 <a11914741@univie.ac.at>
2021-09-04 16:13:22 +0200

    Activities and StartActivity

--- ChatAppFirebase/app/src/main/java/com/example/chatappfirebase/StartActivity.java
+++ ChatAppFirebase/app/src/main/java/com/example/chatappfirebase/StartActivity.java
@@ -2,13 +2,34 @@ package com.example.chatappfirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+    Button login, register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LogInActivity.class));
+            }
+        });
+
+        register = findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        });
+
     }
 }
