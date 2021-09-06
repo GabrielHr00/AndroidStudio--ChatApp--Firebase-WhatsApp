1cb357449cd2352341446b6feb840b9c2a82181d
gabrielh00 <a11914741@univie.ac.at>
2021-09-06 15:57:45 +0200

    Register screen firebase done

--- ChatAppFirebase/app/src/main/java/com/example/chatappfirebase/RegisterActivity.java
+++ ChatAppFirebase/app/src/main/java/com/example/chatappfirebase/RegisterActivity.java
@@ -1,17 +1,35 @@
 package com.example.chatappfirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.rengwuxian.materialedittext.MaterialEditText;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
     MaterialEditText et_username, et_password, et_email;
     Button register;
     Toolbar toolbar;
+    String username, email, password;
+
+    FirebaseAuth mauth;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +44,78 @@ public class RegisterActivity extends AppCompatActivity {
         et_username = findViewById(R.id.reg_username);
         et_email = findViewById(R.id.reg_email);
         et_password = findViewById(R.id.reg_password);
+
+
+        mauth = FirebaseAuth.getInstance();
+
         register = findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = et_email.getText().toString();
+                password = et_password.getText().toString();
+                username = et_username.getText().toString();
+
+                if(TextUtils.isEmpty(email)){
+                    et_email.setError("Required!");
+                }
+                else if(TextUtils.isEmpty(username)){
+                    et_username.setError("Required!");
+                }
+                else if(TextUtils.isEmpty(password)){
+                    et_password.setError("Required");
+                }
+                else if(password.length() < 6){
+                    et_password.setError("Length must be more than 6 chars!");
+                }
+                else{
+                    registerUser(username, password, email);
+                }
+            }
+
+        });
+
+    }
+    private void registerUser(String username, String password, String email) {
+        mauth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser user = mauth.getCurrentUser();
+                    reference = FirebaseDatabase.getInstance().getReference("Users").child(user.getUid());
+
+                    if(user != null){
+                        HashMap<String, Object> hashMap = new HashMap<>();
+                        hashMap.put("username", username);
+                        hashMap.put("email", email);
+                        hashMap.put("id", user.getUid());
+
+                        reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(RegisterActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+
+                                    startActivity(new Intent(RegisterActivity.this,
+                                            StartActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK ));
+
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        });
 
     }
 }
+
+
+
+
+
+
+
+
+
+
