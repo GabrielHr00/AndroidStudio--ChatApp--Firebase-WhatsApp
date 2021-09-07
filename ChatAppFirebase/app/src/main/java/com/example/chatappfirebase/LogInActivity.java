4c123571ddf9452b964df57d882c0b2b4971fd75
gabrielh00 <a11914741@univie.ac.at>
2021-09-07 14:55:41 +0200

    LogIn into the database

--- ChatAppFirebase/app/src/main/java/com/example/chatappfirebase/LogInActivity.java
+++ ChatAppFirebase/app/src/main/java/com/example/chatappfirebase/LogInActivity.java
@@ -1,17 +1,29 @@
 package com.example.chatappfirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.accessibility.AccessibilityViewCommand;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class LogInActivity extends AppCompatActivity {
     MaterialEditText et_email, et_password;
     Button login;
     Toolbar toolbar;
+    String email, password;
+    FirebaseAuth mauth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +37,41 @@ public class LogInActivity extends AppCompatActivity {
 
         et_email = findViewById(R.id.log_email);
         et_password = findViewById(R.id.log_password);
+        mauth = FirebaseAuth.getInstance();
         login = findViewById(R.id.login_account);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = et_email.getText().toString();
+                password = et_password.getText().toString();
+
+                if(TextUtils.isEmpty(email)){
+                    et_email.setError("Required!");
+                }
+                else if(TextUtils.isEmpty(password)){
+                    et_password.setError("Required!");
+                }
+                else{
+                    LoginMeIn(email, password);
+                }
+            }
+        });
+    }
+
+    private void LoginMeIn(String email, String password) {
+        mauth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(LogInActivity.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+                    Toast.makeText(LogInActivity.this, "Logged In successfully!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
