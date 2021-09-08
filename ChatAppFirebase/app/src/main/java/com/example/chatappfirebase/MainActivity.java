2951db2711891601f2199fad0f51a0ce514aadf8
gabrielh00 <a11914741@univie.ac.at>
2021-09-08 16:05:41 +0200

    Fragments and viewpager2

--- ChatAppFirebase/app/src/main/java/com/example/chatappfirebase/MainActivity.java
+++ ChatAppFirebase/app/src/main/java/com/example/chatappfirebase/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.chatappfirebase;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 
+import com.example.chatappfirebase.Fragments.ChatsFragment;
+import com.example.chatappfirebase.Fragments.ProfileFragment;
+import com.example.chatappfirebase.Fragments.UsersFragment;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth mauth;
@@ -25,6 +42,72 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        TabLayout tablayout = findViewById(R.id.tablayout);
+        final ViewPager2 pa = findViewById(R.id.viewPager);
+
+        ViewStateAdapter fragmentAdapter = new ViewStateAdapter(getSupportFragmentManager(), getLifecycle());
+
+        fragmentAdapter.addFragment(new ChatsFragment(), "Chats");
+        fragmentAdapter.addFragment(new UsersFragment(), "Users");
+        fragmentAdapter.addFragment(new ProfileFragment(), "Profile");
+
+        pa.setAdapter(fragmentAdapter);
+        tablayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pa.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        pa.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tablayout.selectTab(tablayout.getTabAt(position));
+            }
+        });
+        //tablayout.setupWithViewPager(viewPager);
+    }
+
+    private class ViewStateAdapter extends FragmentStateAdapter {
+        ArrayList<Fragment> fragments;
+        ArrayList<String> titles;
+
+        public ViewStateAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+            super(fragmentManager, lifecycle);
+            fragments = new ArrayList<>();
+            titles = new ArrayList<>();
+        }
+
+
+        public void addFragment(Fragment fragment, String title){
+            fragments.add(fragment);
+            titles.add(title);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return fragments.size();
+        }
+
+        public CharSequence getPageTitle(int position){
+            return titles.get(position);
+        }
     }
 
     @Override
@@ -41,5 +124,11 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finishAffinity();
     }
 }
