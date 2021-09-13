13f73f9ac1b069ce86a56fbe1ecc70b22cf16673
gabrielh00 <a11914741@univie.ac.at>
2021-09-13 15:46:42 +0200

    Pictures and layout message modifications

--- ChatAppFirebase/app/src/main/java/com/example/chatappfirebase/MessageActivity.java
+++ ChatAppFirebase/app/src/main/java/com/example/chatappfirebase/MessageActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -16,6 +17,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -26,11 +28,16 @@ public class MessageActivity extends AppCompatActivity {
     Toolbar toolbar;
 
     FirebaseUser firebaseUser;
+    MaterialEditText et_message;
+    Button send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        et_message = findViewById(R.id.edit_message_text);
+        send = findViewById(R.id.send_messsage_btn);
 
         //Toolbar
         toolbar = findViewById(R.id.toolbar_message);
